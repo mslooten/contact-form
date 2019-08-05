d9bb3626e4e90af116d31450f8d4fb8bca819c39
mslooten <marco.slooten@topicus.nl>
2019-08-05 20:55:06 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import './style.css';
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+import Form from './Form';
+
+const HeaderMain = styled.h1`
+  font-size: 2.4rem;
+  font-family: sans-serif;
+  text-align: center;
+  margin-bottom: 4rem;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HeaderMain>Contact form</HeaderMain>
+        <Form />
       </header>
     </div>
   );
